5257339f46fa1b451ef3bdf3f16b25f2afca1ca1
jaubut <jeremie.aubut@gmail.com>
2020-03-27 13:52:59 -0400

    update les textes

--- components/grids/grids-5.vue
+++ components/grids/grids-5.vue
@@ -3,11 +3,11 @@
     <div class="row py-5">
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Podcast</h4>
-        <div class="small">Retrouver moi chaque jour pour une Pensée du jour sur <a target="_blank" href="https://soundcloud.com/pensee-du-jour"><span>soundclound</span></a></div>
+        <div class="small">Retrouver moi chaque matin pour la pensée du jour sur <a target="_blank" href="https://soundcloud.com/pensee-du-jour"><span>soundclound</span></a></div>
       </div>
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Vidéos</h4>
-        <div class="small">Un reccueil de tous mes enseignements filmés et reccueillis sur <a target="_blank" href="https://www.youtube.com/channel/UCdiJGksQJ7e_cacatlSmGsw"><span>mon YouTube</span></a>.</div>
+        <div class="small">Un reccueil de tous mes enseignements filmés sur<a target="_blank" href="https://www.youtube.com/channel/UCdiJGksQJ7e_cacatlSmGsw"><span>ma chaîne YouTube</span></a>.</div>
       </div>
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Cours en ligne</h4>
@@ -15,7 +15,7 @@
       </div>
       <div class="col-12 col-md-3">
         <h4 class="text-secondary">Article de blog</h4>
-        <div class="small">Vous aurez aussi accès à des articles écrit sur diverses sujets entourant Jésus, Dieu, la bible et <span class="text-secondary">la culture du Royaume.</span></div>
+        <div class="small">Vous aurez aussi accès à des articles écrit sur diverses sujets entourant Jésus, la bible et <span class="text-secondary">la culture du Royaume.</span></div>
       </div>
     </div>
   </div>
