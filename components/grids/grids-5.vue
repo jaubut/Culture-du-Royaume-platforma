64c4299dad3a6795bcce853f90ac13a318a28e9c
jaubut <jeremie.aubut@gmail.com>
2020-03-27 16:24:33 -0400

    change some text

--- components/grids/grids-5.vue
+++ components/grids/grids-5.vue
@@ -3,11 +3,11 @@
     <div class="row py-5">
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Podcast</h4>
-        <div class="small">Retrouver moi chaque matin pour la pensée du jour sur <a target="_blank" href="https://soundcloud.com/pensee-du-jour"><span>soundclound</span></a></div>
+        <div class="small">Retrouver moi chaque matin pour la pensée du jour sur <a target="_blank" href="https://soundcloud.com/pensee-du-jour"><span>soundclound</span></a>.</div>
       </div>
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Vidéos</h4>
-        <div class="small">Un reccueil de tous mes enseignements filmés sur<a target="_blank" href="https://www.youtube.com/channel/UCdiJGksQJ7e_cacatlSmGsw"><span>ma chaîne YouTube</span></a>.</div>
+        <div class="small">Un reccueil de tous mes enseignements filmés sur <a target="_blank" href="https://www.youtube.com/channel/UCdiJGksQJ7e_cacatlSmGsw"><span>ma chaîne YouTube</span></a>.</div>
       </div>
       <div class="col-12 col-md-3 mb-4 mb-md-0">
         <h4 class="text-secondary">Cours en ligne</h4>
